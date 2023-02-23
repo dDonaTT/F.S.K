36ccf13a08ec49a92146cc2edabf6292fb96e17d
DonaTT <116920667+dDonaTT@users.noreply.github.com>
2023-02-23 22:19:27 +0100

    indexAdded

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php 
+  include('navbar/login-check.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styleindex.css">
+    <link rel="icon" href="img/logo.png">
+    <title>Welcome</title>
+
+</head>
+<body> 
+  <div class="main">
+    <div class="welcome">
+        <div class="outline">
+          <p id="animated-text">Përshendetje!Kjo është webfaqja e Forcës së Sigurisë së Kosovës e krijuar nga Donat Shala.
+            Qëllimi i krijimit të kësaj webfaqeje është që ti mundësoj njerëzve që të mundë të aplikojnë në F.S.K.Për tu futur në webfaqe 
+             së pari duhet të regjistroheni ose nese jeni Administrator i webfaqes kyquni si Administrator!
+            </p>
+            <p><button class="btn-register"><b><a href="./register.php"> Regjistrohuni këtu</b></a></button>  <p><button class="btn-kyquni"><b><a href="admin/login-admin.php">Kyquni këtu si Administrator</b></a></button></p>
+        </div>
+      </div>
+</div>
+      
+    <script src="script.js"></script>
+</body> 
+</html>
